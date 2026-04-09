27b15f2f0b2cb78e9635c26ea397583f1324902b
lamadirbi <lamaadirbi@gmail.com>
2026-04-09 19:59:42 +0300

    Fix API base URL and update frontend UI

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,5 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://147.182.131.131/api";
+  process.env.NEXT_PUBLIC_API_URL ??
+  process.env.NEXT_PUBLIC_API_BASE_URL ??
+  "http://147.182.131.131/api";
 

695c1bcb6f1b30798ebe2455c8dec756d0ad2af4
ismailnaser <ismailnaser67@gmail.com>
2026-04-14 14:58:07 +0300

    update host

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -2,5 +2,5 @@
 // Set this to your backend origin (with or without /api). Example:
 // window.__LAMA_API_URL__ = "https://lama-backend-xyz.ondigitalocean.app";
 // window.__LAMA_API_URL__ = "https://lama-backend-xyz.ondigitalocean.app/api";
-window.__LAMA_API_URL__ = "";
+window.__LAMA_API_URL__ = "https://surgical-dressing-log-z4xd6.ondigitalocean.app/api";
 

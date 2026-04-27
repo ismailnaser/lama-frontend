8d5030178270d111b944a2888f3c48a7d440f8e8
ismailnaser <ismailnaser67@gmail.com>
2026-04-27 23:32:24 +0300

    update

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -3,6 +3,7 @@ import { getAuthToken } from "./auth";
 export async function apiFetch(input: string, init?: RequestInit) {
   const token = getAuthToken();
   const headers = new Headers(init?.headers ?? undefined);
+  if (!headers.has("Accept")) headers.set("Accept", "application/json");
   if (token) headers.set("Authorization", `Bearer ${token}`);
   return fetch(input, { ...init, headers });
 }
